0e7956e6f5f341f699180cde6c18bdff7996ddc5
Adam Griffiths <adam.lw.griffiths@gmail.com>
2015-09-07 12:02:00 +1000

    Strip dependencies, version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='hmac_signer',
-    version='0.1',
+    version='1.0',
     author='C Bates',
     author_email='chrsbats@gmail.com',
     packages=['hmac_signer'],
@@ -12,13 +12,8 @@ setup(
     description='HMAC Sign your Requests posts and validate them in Flask',
     long_description='',
     install_requires=[
-        "Flask==0.10.1",
-        "itsdangerous==0.24",
-        "Jinja2==2.7.3",
-        "MarkupSafe==0.23",
-        "python-dateutil==2.4.2",
-        "requests==2.7.0",
-        "six==1.9.0",
-        "Werkzeug==0.10.4"
+        "Flask>=0.10.1",
+        "python-dateutil>=2.4.2",
+        "requests>=2.7.0",
     ],
 )
