ddd59eeb84610657533fe7191553e8d98267a424
bats <chris.bates@seer.net.au>
2016-03-04 15:04:25 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='hmac_signer',
-    version='1.0',
+    version='1.0.1',
     author='C Bates',
     author_email='chrsbats@gmail.com',
     packages=['hmac_signer'],
